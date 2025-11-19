b3bc6ff68e405635dfd33f8beba343ac5663ae99
John <john@Johns-MacBook-Pro.local>
2025-11-19 19:12:00 +1100

    Test manual PR comment posting functionality

--- test-secrets.tf
+++ test-secrets.tf
@@ -1,5 +1,6 @@
 # Test file for secret detection
 # This file contains fake AWS credentials for testing purposes
+# Testing manual PR comment posting v5
 
 variable "aws_access_key_id" {
   description = "AWS Access Key ID"
