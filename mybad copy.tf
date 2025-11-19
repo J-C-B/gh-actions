4839ea28a19a404f474e46f9c86d62d3176b3a09
John <john@Johns-MacBook-Pro.local>
2025-11-19 16:44:39 +1100

    Update Terraform files

---
+++ mybad copy.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+resource "aws_security_group" "bad_example" {
+  name        = "bad_example_sg"
+  description = "Unrestricted bad Security Group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # All protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # All protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bad_example_sg"
+  }
+}
