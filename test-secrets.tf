3e71324523cab88e45488d72e07a036c98b4d8ea
John <john@Johns-MacBook-Pro.local>
2025-11-19 21:40:46 +1100

    Add new secrets types for Lacework testing

--- test-secrets.tf
+++ test-secrets.tf
@@ -1,6 +1,6 @@
 # Test file for secret detection
 # This file contains fake AWS credentials for testing purposes
-# Testing native Lacework PR comments with token input (v8)
+# Testing native Lacework PR comments with token input - v9
 
 variable "aws_access_key_id" {
   description = "AWS Access Key ID"
@@ -33,3 +33,41 @@ resource "aws_instance" "test" {
   EOF
 }
 
+# New secrets for testing - using fake patterns that won't trigger GitHub push protection
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "test-bucket-12345"
+  
+  # GitHub token example (fake pattern)
+  tags = {
+    github_token = "ghp_TEST1234567890abcdefghijklmnopqrstuvwxyzFAKE"
+    api_key      = "sk_test_FAKE1234567890abcdefghijklmnopqrstuvwxyz"
+  }
+}
+
+# Database credentials
+resource "aws_db_instance" "test_db" {
+  identifier = "test-db"
+  engine     = "postgres"
+  
+  # Hardcoded database password
+  password = "TestPassword123!@#FAKE"
+  username = "admin"
+}
+
+# API keys in variables (using fake patterns)
+variable "stripe_secret_key" {
+  default = "sk_test_FAKE1234567890abcdefghijklmnopqrstuvwxyz"
+}
+
+variable "slack_webhook_url" {
+  default = "https://hooks.slack.com/services/TEST00000/BTEST00000/FAKETESTTOKEN123456789"
+}
+
+# Additional secret types
+variable "jwt_token" {
+  default = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkZha2UgVGVzdCJ9.FAKE"
+}
+
+variable "private_key" {
+  default = "-----BEGIN RSA PRIVATE KEY-----\nFAKEKEYDATAHERE1234567890\n-----END RSA PRIVATE KEY-----"
+}
